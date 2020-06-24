6628afb48f2b1b3b9985094f80e4b5020aaea365
st875052018 <st875052007@gmail.com>
2020-06-24 16:31:53 +0800

    react done

---
+++ build/precache-manifest.cacb3f7a755096cee8e2065d205566c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730c365377eaf1831610eb5620f67bb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fe1e5e584c753c3735c",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d68d1a0188ec72701cbe",
+    "url": "/static/css/main.dd6b3699.chunk.css"
+  },
+  {
+    "revision": "5fe1e5e584c753c3735c",
+    "url": "/static/js/2.82e4e86b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82e4e86b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68d1a0188ec72701cbe",
+    "url": "/static/js/main.3d849f32.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
