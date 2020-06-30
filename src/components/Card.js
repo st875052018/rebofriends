c6409220b668a1487b5df1fcc8428bc78c026949
st875052018 <st875052007@gmail.com>
2020-06-30 12:02:53 +0800

    robocat is online

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
 	return (
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+		<div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img alt='robots' src={`https://robohash.org/${id}?set=set4`} />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
